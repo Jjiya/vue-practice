8ca1f3718edb238cf7f99abd5f82bc347f1c7df5
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-02-17 01:56:05 +0900

    feat - 3-4. v-for로 반복문 사용하기
    - v-for 배우기

--- zero-cho/3. 숫자야구/numberBaseball.vue
+++ zero-cho/3. 숫자야구/numberBaseball.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>{{ result }}</h1>
-    <form v-on:submit="onSubmitForm">
+    <form @submit.prevent="onSubmitForm"> <!-- v-on >> @로 변환 가능, event.preventDefault를 여기서 .prevent를 추가함으로써 적용 가능 -->
       <input ref="answer" maxlength="4" v-model="value">
       <button>입력</button>
     </form>
 
-    <div>시도: {{value}}</div>
+    <div> 시도: {{ tries.length }}</div>
+    <ul>
+      <li v-for="trying in tries">
+        <div>{{ trying.tryNumber }}</div>
+        <div>- {{ trying.result }}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,13 +20,19 @@
 export default {
   data() {
     return {
+      tries: [],
       value: "",
       result: ""
     };
   },
   methods: {
-    onSubmitForm(e) {
-      e.preventDefault();
+    onSubmitForm() {
+      this.tries.push({
+        tryNumber: this.value,
+        result: "홈런"
+      });
+      this.value = "";
+      this.$refs.answer.focus();
     }
   }
 }
