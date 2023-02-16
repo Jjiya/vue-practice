a669bc4cc61f1ad8aa0b41021a5977caf82e44e0
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-02-17 02:27:35 +0900

    feat - 3-5. 숫자야구 완성하기

--- zero-cho/3. 숫자야구/numberBaseball.vue
+++ zero-cho/3. 숫자야구/numberBaseball.vue
@@ -6,6 +6,13 @@
       <button>입력</button>
     </form>
 
+    <div>
+      <p>기록</p>
+      <div v-for="idx in [1,2,3,4]">
+        {{ idx }}번째 수: <input>
+      </div>
+    </div>
+
     <div> 시도: {{ tries.length }}</div>
     <ul>
       <li v-for="trying in tries">
@@ -17,9 +24,21 @@
 </template>
 
 <script>
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let array = [];
+
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+
+  return array;
+}
 export default {
   data() {
     return {
+      answer: getNumbers(),
       tries: [],
       value: "",
       result: ""
@@ -27,12 +46,51 @@ export default {
   },
   methods: {
     onSubmitForm() {
+      let result;
+      if (this.value === this.answer.join(",")) {
+        result = "홈런";
+        this.result = result;
+
+        this.resetGame();
+      } else {
+        if (this.tries.length === 9) {
+        this.result = `10회 실패. 정답은 ${this.answer.join(",")}입니다! 5초 뒤 다시 시작합니다.`;
+
+          setTimeout(() => {
+            this.resetGame();
+          }, 5000);
+        }
+
+        let ball = 0;
+        let strike = 0;
+
+        [...this.value].forEach((number, idx) => {
+          number = parseInt(number);
+
+          if (number === this.answer[idx]) {
+            strike++;
+          } else if (this.answer.includes(number)) {
+            ball++;
+          }
+        });
+
+        result = `${strike} 스트라이크, ${ball}볼입니다.`;
+      }
+
       this.tries.push({
         tryNumber: this.value,
-        result: "홈런"
+        result,
       });
+
       this.value = "";
       this.$refs.answer.focus();
+    },
+    resetGame() {
+      this.value = "";
+      this.result = "";
+      this.tries = [];
+      this.answer = getNumbers();
+      alert("게임을 다시 시작합니다.");
     }
   }
 }
