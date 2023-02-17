e2d3e91dc886d09ebf760565eaa98b193af74011
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-02-17 20:18:49 +0900

    feat - 반응속도체크 게임 완성하기
    - 넣고싶은 기능 조금 더 넣어서 코드가 강의와 다를 수 있음

--- zero-cho/4. 반응속도체크/ResponseCheck.vue
+++ zero-cho/4. 반응속도체크/ResponseCheck.vue
@@ -2,13 +2,23 @@
   <div>
     <div id="screen" v-bind:class="state" @click="onClickScreen">{{ message }}</div>
     <div>
-      <div>평균 시간: {{}}</div>
+      <div>평균 시간: {{ (result.reduce((sum, time) => sum + time, 0) / result.length) || 0 }}ms</div>
       <button @click="onReset">리셋</button>
+    </div>
+    <div>
+      기록 {{result.length}}회
+      <ol>
+        <li v-for="rs in result">{{rs}}ms</li>
+      </ol>
     </div>
   </div>
 </template>
 
 <script>
+let startTime = 0;
+let endTime = 0;
+let stateToNowTimeout = null;
+
 export default {
   data() {
     return {
@@ -19,20 +29,41 @@ export default {
   },
   methods: {
     onReset() {
+      this.result = [];
     },
     onClickScreen() {
       switch (this.state) {
         case "waiting":
-          this.state = "now";
-          break;
-        case "now":
           this.state = "ready";
+          this.message = "초록색이 되면 클릭하세요.";
+
+          stateToNowTimeout = setTimeout(() => {
+            this.state = "now";
+            this.message = "지금 클릭하세요!";
+            startTime = new Date();
+          }, Math.floor(Math.random() * 1000) + 2000);
+
           break;
         case "ready":
+          clearTimeout(stateToNowTimeout);
+
           this.state = "waiting";
+          this.message = "너무 성급하시군요! 초록색이 된 후에 클릭하세요.";
+
+          setTimeout(() => {
+            this.onClickScreen();
+          }, 1500);
+          break;
+
+        case "now":
+          endTime = new Date();
+          this.state = "waiting";
+          this.message = "클릭해서 시작하세요.";
+
+          this.result.push(endTime - startTime);
           break;
       }
-    }
+    },
   }
 }
 </script>
