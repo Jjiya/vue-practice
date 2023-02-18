71d4b137ece24cb8be8f97d8a643920524a70c06
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-02-18 14:42:01 +0900

    feat - 5-2. :class와 :style

--- zero-cho/5. 가위바위보/RockPaperScissors.vue
+++ zero-cho/5. 가위바위보/RockPaperScissors.vue
@@ -1,15 +1,46 @@
 <template>
+  <!-- v-on => @ 축약 가능 / v-bind => : 축약 가능 -->
+  <!--  v-bind의 class와 style은 객체 형식 지원 -->
+  <!--  <div id="computer" :class="{state: true, hello: false}" :style="{backgroundImage:'',fontSize:'14px'}"></div>-->
+  <div id="computer" :style="{...computedStyleObject}"></div>
+
+  <div>
+    <button @click="onClickButton('바위')">바위</button>
+    <button @click="onClickButton('가위')">가위</button>
+    <button @click="onClickButton('보')">보</button>
+  </div>
+  <div>{{ result }}</div>
+  <div>현재 {{ score }}점</div>
 </template>
 
 <script>
+const rspCoordinate = {
+  "바위": "0",
+  "가위": "-142px",
+  "보": "-284px"
+};
+
 export default {
   data() {
     return {
+      result: "",
+      score: 0,
+      imgCoordinate: rspCoordinate.바위,
     };
   },
   computed: {
+    computedStyleObject() {
+      return {
+        background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoordinate} 0`,
+        width: "150px",
+        height: "243px"
+      }
+    }
   },
   methods: {
+    onClickButton(choice) {
+      this.imgCoordinate = rspCoordinate[choice];
+    }
   }
 }
 </script>
