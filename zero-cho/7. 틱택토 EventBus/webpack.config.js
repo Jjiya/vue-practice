c849f1e6ac044892858fdaa9867ec9b6ec205c70
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-02-22 02:07:32 +0900

    feat - 7-5. EventBus 사용하기
    - vue 3에서는 eventbus를 지원하지 않기 때문에 store로 변경함

---
+++ zero-cho/7. 틱택토 EventBus/webpack.config.js
@@ -0,0 +1,41 @@
+const {VueLoaderPlugin} = require("vue-loader");
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  devtool: "eval",
+  resolve: {  //
+    extensions: [".js", ".vue"],
+  },
+  entry: {
+    app: path.join(__dirname, "main.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+    publicPath: "/dist",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/')
+    }
+  },
+};
