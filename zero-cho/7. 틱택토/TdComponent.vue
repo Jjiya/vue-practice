05b9518e92012a4191a33de4b348421f4d0e8b4c
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-02-22 00:14:54 +0900

    feat - 7-3. Vue.set
    - vue3 이전에는 배열 index를 통해 값 변경 시 인식하지 못했다.

--- zero-cho/7. 틱택토/TdComponent.vue
+++ zero-cho/7. 틱택토/TdComponent.vue
@@ -12,15 +12,29 @@ export default {
   },
   methods: {
     onClickTd() {
-      console.log(this.$root.$data);  // 최상위 root data 접근
-      console.log(this.$parent.$data);  // 상위 parent data 접근
-
-      let turning = {
+      // console.log(this.$root.$data);  // 최상위 root data 접근
+      // console.log(this.$parent.$data);  // 상위 parent data 접근
+      const turning = {
         "O": "X",
         "X": "O",
       };
-      let {turn} = this.$root.$data;
+      let {turn, tableData} = this.$root.$data;
 
+      tableData[this.rowIndex][this.cellIndex] = turn;
+      /*
+        원래는 배열 data의 index에 접근해서 값을 바꾸면 vue가 자동 렌더링 되지 않았는데
+        vue3로 업데이트되면서 개선된 것 같음
+
+        원래 tableData[1][0] = "X" 같은 경우 작동하지 않아서
+        vue의 set함수를 통해 변경해줘야 data 변경을 감지할 수 있었음
+        => data변경 감지 시 주소 값을 통해 감지하는데,
+           index를 통한 접근은 주소에 들어있는 배열 값이 실질적으로 변경되지 않았기 때문이다.
+           (vue가 가리키고 있는 주소 내의 주소 값에 들어가야 값이 변경된 것을 알 수 있기 때문)
+
+        e.g) 1. this.$set(this.tableData[1], 0, "X");
+        2-1. import Vue from "vue";
+        2-2. Vue.set(this.tableData[1], 0, "X");
+      */
       this.$root.$data.turn = turning[turn];
     }
   }
