7e83df99cef6422f46f51a9135bf1ac42f140e2b
Jjiya <66312330+Jjiya@users.noreply.github.com>
2023-02-23 17:14:11 +0900

    feat - 8-3. 데이터 가공해 화면 그리기

--- zero-cho/8.지뢰찾기/TableComponent.vue
+++ zero-cho/8.지뢰찾기/TableComponent.vue
@@ -1,19 +1,68 @@
 <template>
   <table>
     <tr v-for="(rowData, rowIndex) in tableData" :key="'mine-row' + rowIndex">
-      <td v-for="(cellData, cellIndex) in rowData" :key="'mine-cell' + cellIndex">{{ cellData }}</td>
+      <td v-for="(cellData, cellIndex) in rowData"
+          :key="'mine-cell' + cellIndex"
+          :style="cellDataStyle(rowIndex, cellIndex)"
+      >
+        {{ cellDataText(rowIndex, cellIndex) }}
+      </td>
     </tr>
   </table>
 </template>
 
 <script>
 import {mapState} from "vuex";
-import store from "./store";
+import store, {CODE} from "./store";
 
 export default {
   store,
   computed: {
     ...mapState(["tableData"]),
+    cellDataStyle: (state) => (rowIndex, cellIndex) => {
+      let style = {
+        background: "#fff"
+      }
+      switch (state.tableData[rowIndex][cellIndex]) {
+        case CODE.OPENED:
+        case CODE.CLICKED_MINE:
+          style.background = "#fff";
+          break;
+        case CODE.NORMAL:
+        case CODE.MINE:
+          style.background = "#666";
+          break;
+        case CODE.QUESTION:
+        case CODE.QUESTION_MINE:
+          style.background = "yellow";
+          break;
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+          style.background = "red";
+          break;
+      }
+
+      return style;
+    },
+    cellDataText: state => (rowIndex, cellIndex) => {
+      switch (state.tableData[rowIndex][cellIndex]) {
+        case CODE.CLICKED_MINE:
+          return "펑";
+        case CODE.MINE:
+          return "💣";
+        case CODE.OPENED:
+        case CODE.NORMAL:
+          return ""
+        case CODE.QUESTION:
+        case CODE.QUESTION_MINE:
+          return "?";
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+          return "!";
+        default:
+          return "";
+      }
+    },
   }
 };
 </script>
